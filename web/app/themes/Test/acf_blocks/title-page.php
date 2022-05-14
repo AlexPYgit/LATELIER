b52e9f5a3a06c7860178be417805313acafa4459
AlexPY <alexis.payet0@gmail.com>
2022-05-14 16:54:05 +0200

    feat: style of slider modify

---
+++ web/app/themes/Test/acf_blocks/title-page.php
@@ -0,0 +1,17 @@
+<div class="block-section">
+    <div class="block-container">
+        <div>
+            <div>
+                <h1><?php get_field('title-page') ?></h1>
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <img src="<?= get_template_directory_uri(). '/assets' ?>" alt="decoration">
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+</div>
